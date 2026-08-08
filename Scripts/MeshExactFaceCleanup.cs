cb8b7565dd5a9fc5797e6a8616d5a2025892a123
Vyacheslav Maltsev <34792447+Trafalgardi@users.noreply.github.com>
2026-08-08 23:03:40 +0700

    Refresh submesh bounds after cleanup

--- Scripts/MeshExactFaceCleanup.cs
+++ Scripts/MeshExactFaceCleanup.cs
@@ -183,7 +183,7 @@ internal static class MeshExactFaceCleanup
                 filteredIndices.Add(sourceIndices[triangleStart + 2]);
             }
 
-            mesh.SetIndices(filteredIndices, MeshTopology.Triangles, subMeshIndex, false, 0);
+            mesh.SetIndices(filteredIndices, MeshTopology.Triangles, subMeshIndex, true, 0);
         }
 
         mesh.RecalculateBounds();
